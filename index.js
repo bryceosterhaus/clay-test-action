b4232ac4c212919c7c7b11efcb1a19a301d6de72
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-09 15:56:37 -0700

    will squash before merge:

--- index.js
+++ index.js
@@ -28,23 +28,27 @@ async function run() {
 		const octokit = new github.GitHub(github_token);
 
 
-		octokit.pulls.list({repo: 'clay', owner: 'bryceosterhaus'}).forEach(pr => {
-			if (
-				!pr.labels.find(label => label.name === 'Build Stats Finished')
-			) {
-				octokit.issues.createComment({
-					...context.repo,
-					body: 'test',
-					issue_number: pr.number,
-				});
+		octokit.pulls.list({repo: 'clay', owner: 'bryceosterhaus'}).then(
+			prs => {
+				prs.forEach(pr => {
+					if (
+						!pr.labels.find(label => label.name === 'Build Stats Finished')
+					) {
+						octokit.issues.createComment({
+							...context.repo,
+							body: 'test',
+							issue_number: pr.number,
+						});
 
-				octokit.issues.addLabels({
-					...context.repo,
-					issue_number: pr.number,
-					labels: 'Build Stats Finished',
-				});
-			}
-		});
+						octokit.issues.addLabels({
+							...context.repo,
+							issue_number: pr.number,
+							labels: 'Build Stats Finished',
+						});
+					}
+				})
+			});
+
 	} catch (error) {
 		core.setFailed(error.message);
 	}
