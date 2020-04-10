1da2dd86ec5481629b731a04ee2a8990db7a5f28
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-09 17:06:04 -0700

    will squash before merge:

--- index.js
+++ index.js
@@ -19,26 +19,17 @@ async function run() {
 
 		const octokit = new github.GitHub(github_token);
 
-
-
-
-
-
-
-
-
-
 		octokit.pulls.list(context.repo).then(
 			prs => {
 				prs.data.forEach(pr => {
 					if (
 						!pr.labels.find(label => label.name === 'Build Stats Finished')
 					) {
-						octokit.issues.addLabels({
-							...context.repo,
-							issue_number: pr.number,
-							labels: ['Build Stats Started'],
-						});
+						// octokit.issues.addLabels({
+						// 	...context.repo,
+						// 	issue_number: pr.number,
+						// 	labels: ['Build Stats Started'],
+						// });
 
 						// Build stats...
 
@@ -48,11 +39,11 @@ async function run() {
 							issue_number: pr.number,
 						});
 
-						octokit.issues.removeLabel({
-							...context.repo,
-							issue_number: pr.number,
-							name: 'Build Stats Started'
-						});
+						// octokit.issues.removeLabel({
+						// 	...context.repo,
+						// 	issue_number: pr.number,
+						// 	name: 'Build Stats Started'
+						// });
 
 						octokit.issues.addLabels({
 							...context.repo,
